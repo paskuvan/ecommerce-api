ccced7ad7bf322ca22c4f0c0c5f6e34cfdaea234
Paskuvan <majopaskuvan@gmail.com>
2022-01-19 18:18:32 -0300

    routes

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require("./routes/user")
 
 dotenv.config();
 
 mongoose
 .connect(process.env.MONGO_URL,)
-.then(() => console.log("DB Connection Successfull!")).catch((err) => console.log(err));
+.then(() => console.log("DB Connection Successfull!"))
+.catch((err) => 
+console.log(err));
 
-app.listen(5000, () => {
+app.use("/api/user", userRoute);
+
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is running on port 5000");
 });
