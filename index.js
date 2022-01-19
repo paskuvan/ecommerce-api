fa4aebe2f2253a379213d85fee8600d3db698c25
Paskuvan <majopaskuvan@gmail.com>
2022-01-19 16:35:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+
+app.listen(5000, () => {
+    console.log("Backend is running on port 5000");
+});
