00b868ecd401475a42c7581ea9131ac037e657fe
Paskuvan <majopaskuvan@gmail.com>
2022-01-19 17:49:06 -0300

    env

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
+mongoose
+.connect(process.env.MONGO_URL,)
+.then(() => console.log("DB Connection Successfull!")).catch((err) => console.log(err));
 
 app.listen(5000, () => {
     console.log("Backend is running on port 5000");
