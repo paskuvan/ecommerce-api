d4cb0a3a4a4a562cd3093f937e188f0e5d7db2df
Paskuvan <majopaskuvan@gmail.com>
2022-01-23 23:41:06 -0300

    update user

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
-mongoose.connect("mongodb+srv://user01:elmita19@cluster0.nu2dq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-.then(() => console.log("DBConnection Successfull!"))
-.catch((err) => {
-    console.log(err);
-});
+dotenv.config();
 
-app.listen(5000, () => {
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBConnection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+    app.use(express.json());
+
+    app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
 });
